32cb833baac417f47dd15e581c36c382db771e1b
matcool <26722564+matcool@users.noreply.github.com>
2022-09-17 15:44:22 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# gd-mod-cpm
+
+geometry dash mod template using [CPM.cmake](https://github.com/cpm-cmake/CPM.cmake) as a package manager
+
+this also uses [mat-dash](https://github.com/matcool/mat-dash) to make hooking syntax simpler (and mod_main (very nice))
+
+## setup
+
+clone and configure/build with cmake using x86 msvc (too lazy to copy paste the 32 bit clang setup sorry)
+
+## tip
+
+with how cpm works if you want to avoid recloning the same repo multiple times you can look into [CPM_SOURCE_CACHE](https://github.com/cpm-cmake/CPM.cmake#cpm_source_cache)
+
+## help
+
+for more detailed info for how modding gd works i suggest checking out my [gd-mod-example](https://github.com/matcool/gd-mod-example) repo
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <matdash.hpp>
+
+// defines add_hook to use minhook
+#include <matdash/minhook.hpp>
+
+// lets you use mod_main
+#include <matdash/boilerplate.hpp>
+
+// gd.h includes cocos2d.h
+#include <gd.h>
+
+void MenuLayer_onNewgrounds(gd::MenuLayer* self, cocos2d::CCObject* sender) {
+    std::cout << "cool!" << std::endl;
+    matdash::orig<&MenuLayer_onNewgrounds>(self, sender);
+}
+
+bool GJDropDownLayer_init(gd::GJDropDownLayer* self, const char* title, float height) {
+    return matdash::orig<&GJDropDownLayer_init>(self, "my own title", height * 0.5f);
+}
+
+matdash::cc::thiscall<void> PlayLayer_update(gd::PlayLayer* self, float dt) {
+    matdash::orig<&PlayLayer_update>(self, dt * 0.5f);
+    return {};
+}
+
+bool MenuLayer_init(gd::MenuLayer* self) {
+    if (!matdash::orig<&MenuLayer_init>(self)) return false;
+
+    auto* sprite = cocos2d::CCSprite::create("dialogIcon_017.png");
+    sprite->setPosition(ccp(100, 100));
+    sprite->setScale(0.5f);
+    self->addChild(sprite);
+
+    auto* label = cocos2d::CCLabelBMFont::create("Hello world!", "bigFont.fnt");
+    label->setPosition(ccp(100, 150));
+    label->setAnchorPoint(ccp(0, 0.5));
+    self->addChild(label);
+
+    return true;
+}
+
+void mod_main(HMODULE) {
+    matdash::add_hook<&MenuLayer_onNewgrounds>(gd::base + 0x191e90);
+    matdash::add_hook<&GJDropDownLayer_init>(gd::base + 0x113530);
+    matdash::add_hook<&PlayLayer_update>(gd::base + 0x2029c0);
+    matdash::add_hook<&MenuLayer_init>(gd::base + 0x1907B0);
+}
